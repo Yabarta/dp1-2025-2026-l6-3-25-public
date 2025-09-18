4690d4efbc0f04242a4d2ccdd0dd17a4d1336bfb
josbardel <josbardel1@alum.us.es>
2025-09-18 13:35:28 +0200

    Añadir logo a la página de incio Fixes #3

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
+import logo from '../static/images/petris3D_recortado.png'
 
 export default function Home(){
     return(
         <div className="home-page-container">
             <div className="hero-div">
-                <h1>Your game</h1>
-                <h3>---</h3>
+                <h1>Petris</h1>
+                <img src={logo} width={255} height={369} alt=""/>
                 <h3>Do you want to play?</h3>                
             </div>
         </div>
