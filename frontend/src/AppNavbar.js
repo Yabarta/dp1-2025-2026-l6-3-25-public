56637f4e8e1bb1ef0c60348c397140daf08b82dc
Colanin3 <joseaag05@gmail.com>
2026-01-08 17:42:05 +0100

    WebSocket para friend y eliminacion de console.log

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 import tokenService from './services/token.service';
 import jwt_decode from "jwt-decode";
 import mitosisImg from './static/images/mitosis.png';
-import useFetchState from "./util/useFetchState";
-import {Table } from "reactstrap";
+import FriendsSidebar from './offcanvas/FriendSideBar';
 
 function AppNavbar() {
     const [roles, setRoles] = useState([]);
@@ -13,225 +12,37 @@ function AppNavbar() {
     const jwt = tokenService.getLocalAccessToken();
     const [collapsed, setCollapsed] = useState(true);
     const [isOpenFriends, setIsOpenFriends] = useState(false);
-    const [nombreBuscadoFriend , setNombreFriend] = useState("");
-    const [nombreBuscadoPlayer , setNombrePlayer] = useState("");
-    const [friends, setFriends] = useState([]);
-    const [change , setChange] = useState(false);
-
-
-    //Friend List
-
-    const filterFriend = friends.filter((friend) => friend.requester.nickname === username
-    ?friend.receiver.nickname.toLowerCase().includes(nombreBuscadoFriend.toLowerCase())
-    :friend.requester.nickname.toLowerCase().includes(nombreBuscadoFriend.toLowerCase())
-    );
-
-const friendList = filterFriend.map((friend) => {
-    const friendDisplayName = (friend.receiver.nickname === username) 
-        ? friend.requester.nickname
-        : friend.receiver.nickname;
-
-    return (
-        <tr key={friend.id}>
-            {/* Aquí mostramos el nombre calculado */}
-            <td>{friendDisplayName}</td> 
-            
-            <td>
-                <ButtonGroup>
-                    <Button
-                        style={{justifyContent: 'flex-end', backgroundColor: 'red'}}
-                        onClick={() => {handleDelete(friend.id);}}>
-                        Delete Friend
-                    </Button>
-                </ButtonGroup>
-            </td>
-        </tr>
-    );
-});
+                
+    const toggleNavbar = () => setCollapsed(!collapsed);
+    const toggleMenu = () => setIsOpenFriends(!isOpenFriends);
+    const [id, setId] = useState("");
+    // Deteccion de roles y username
 
     useEffect(() => {
         if (jwt) {
             setRoles(jwt_decode(jwt).authorities);
             setUsername(jwt_decode(jwt).sub);
-        }
+    }
     }, [jwt]);
 
+    // Deteccion de id
     useEffect(() => {
-    if (!username) return;
-
-    const fetchFriends = async () => {
-        try {
-            // A. Hacemos la petición
-            const response = await fetch(`/api/v1/players/${username}/friends`);
-            if (!response.ok) throw new Error("Error en la petición");
-            
+        async function idPlayer() {
+            const response = await fetch(`http://localhost:8080/api/v1/players/nickname/${username}`);
             const data = await response.json();
-
-            // B. Aquí guardamos los amigos en el estado
-            setFriends(data);
-            
-        } catch (error) {
-            console.error(error);
+            setId(data.id);
         }
-    };
-
-    fetchFriends();
-
-}, [username , change]);
-
-    // Request List
-
-    const [requests, setRequests] = useState([]);
-
-    const handleDelete = async(id) => {
-        const response = await fetch(`api/v1/friends/${id}`, {method: 'DELETE', 
-                            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${jwt}`
-            }
-                        });
-        setChange(!change);
-    };
-
-    const handleAccept = async(id) => {
-        const response = await fetch(`api/v1/players/friends/${id}`, {method: 'PUT', 
-                            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${jwt}`
-            }
-                        });
-        setChange(!change);
-    };
-
-    const requestList = requests.map((request) => {
-
-    return (
-        <tr key={request.id}>
-            <td>{request.requester.nickname}</td> 
-            
-            <td>
-                <ButtonGroup>
-                    <Button
-                        style={{justifyContent: 'flex-end', backgroundColor: 'green'}}
-                        onClick={() => {handleAccept(request.id);}}>
-                        Accept
-                    </Button>
-                    <Button
-                        style={{justifyContent: 'flex-end', backgroundColor: 'red'}}
-                        onClick={() => {handleDelete(request.id);}}>
-                        Decline
-                    </Button>
-                </ButtonGroup>
-            </td>
-        </tr>
-    );
-});
-
-useEffect(() => {
-    if (!username) return;
-
-    const fetchRequest = async () => {
-        try {
-            const response = await fetch(`/api/v1/players/${username}/requests`);
-            if (!response.ok) throw new Error("Error en la petición");
-            
-            const data = await response.json();
-            setRequests(data);
-            
-        } catch (error) {
-            console.error(error);
+        if (username){
+            idPlayer();
         }
-    };
-
-    fetchRequest();
-
-}, [username , change]);
-
-    // Player List
-
-    const [players, setPlayers] = useFetchState(
-        [],
-        `/api/v1/players`,
-    );
-
-    const[requester, setRequester] = useState([]);
-
-    useEffect(() => {
-    if (!username) return;
-
-    const fetchRequester = async () => {
-        try {
-            const response = await fetch(`/api/v1/players/${username}/requester`);
-            if (!response.ok) throw new Error("Error en la petición");
-            
-            const data = await response.json();
-            setRequester(data);
-            
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    fetchRequester();
-
-}, [username , change]);
-
-    const filterPlayers = players.filter((player) => {
-        return player.nickname.toLowerCase().includes(nombreBuscadoPlayer.toLowerCase()) && 
-        player.nickname !== username && nombreBuscadoPlayer!== "" && 
-        !friendList.map(friend => friend.props.children[0].props.children).includes(player.nickname) &&  
-        !requestList.map(request => request.props.children[0].props.children).includes(player.nickname);
-    }
-    );
-
-    const handleCreate = async(requester, receiver) => {
-        const payload = { requester, receiver };
-
-    const response = await fetch(`/api/v1/players/friends`, { 
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${jwt}`
-        },
-        body: JSON.stringify(payload)
-    });
-        setChange(!change);
-    };
-
-    const userList = filterPlayers.map((player) => {
-
-        let solicitudEnviada = false;
-
-        if (requester.some(req => req.receiver.nickname === player.nickname)) {
-            solicitudEnviada = true;
-        }
-
-        return (
-        <tr key={player.id}>
-            <td>{player.nickname}</td>
-            <td>
-            <Button
-                color="primary"
-                style={{justifyContent: 'flex-end'}}
-                onClick={() => {handleCreate(username, player.nickname)}}
-                disabled={solicitudEnviada}>
-                {solicitudEnviada ? "Sent Request" : "Send Friends Request"}
-            </Button>
-            </td>
-        </tr>
-            );
-        });
-
-    const toggleNavbar = () => setCollapsed(!collapsed);
-
-    const toggleMenu = () => setIsOpenFriends(!isOpenFriends);
+    }, [username]);
 
     let adminLinks = <></>;
     let ownerLinks = <></>;
     let userLinks = <></>;
     let userLogout = <></>;
     let publicLinks = <></>;
-
+    
     roles.forEach((role) => {
         if (role === "ADMIN") {
             adminLinks = (
@@ -245,6 +56,22 @@ useEffect(() => {
                     
                 </>
             )
+        }
+        else {
+            userLinks = (
+            
+            <>
+                <NavItem>
+                    <Button style={{ color: "white" }} id="friends-btn" onClick={toggleMenu} className="btn btn-link nav-link">Friends</Button>
+                </NavItem>
+                <NavItem>
+                    <NavLink style={{ color: "white" }} id="comparator" tag={Link} to="/comparator">Comparador</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink style={{ color: "white" }} id="statistics" tag={Link} to="/ranking">Ranking</NavLink>
+                </NavItem>
+
+            </>)
         }
     })
 
@@ -261,20 +88,6 @@ useEffect(() => {
             </>
         )
     } else {
-        userLinks = (
-            <>
-                <NavItem>
-                    <Button style={{ color: "white" }} id="friends-btn" onClick={toggleMenu} className="btn btn-link nav-link">Friends</Button>
-                </NavItem>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="comparator" tag={Link} to="/comparator">Comparador</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="statistics" tag={Link} to="/ranking">Ranking</NavLink>
-                </NavItem>
-
-            </>
-        )
         userLogout = (
             <>
                 <NavbarText style={{ color: "white" }} className="justify-content-end">{username}</NavbarText>
@@ -306,65 +119,15 @@ useEffect(() => {
                     </Nav>
                 </Collapse>
             </Navbar>
-
-            <Offcanvas isOpen={isOpenFriends} onClose={toggleMenu} direction='start'  style={{width: "33%" , overflowY: "scroll", borderLeftColor: "white"}} className="bg-dark text-white">
-                <div className="barra-busqueda-Friends d-flex justify-content-center align-items-center position-relative p-3">
-                    <input type="search" value={nombreBuscadoFriend} onChange={(usuario) => setNombreFriend(usuario.target.value)} placeholder="Buscar usuario" />
-                    <Button color="secondary" onClick={toggleMenu} style={{width: '10%'}} className= "position-absolute end-0">X</Button>
-                </div>
-                <div>
-        <Table borderless className="m-0" 
-        style={{
-        "--bs-table-bg": "transparent",
-        "--bs-table-color": "white"
-    }}>
-        <thead>
-            <tr>
-                <th>Username</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-            <tbody>{friendList}</tbody>
-        </Table>
-        </div>
-            </Offcanvas>
-
-            <Offcanvas isOpen={isOpenFriends} onClose={toggleMenu} direction='end' style={{width: "33%", overflowY: "scroll"}} backdrop={false} className="bg-dark text-white">
-                <div className="barra-busqueda-Players d-flex justify-content-center align-items-center position-relative p-3" >
-                    <input type="search" value={nombreBuscadoPlayer} onChange={(usuario) => setNombrePlayer(usuario.target.value)} placeholder="Buscar usuario" />
-                </div>
-                <div style={{ height: "45%", overflowY: "auto", borderBottom: "1px solid #444" }}>
-        <Table aria-label="users" className="mt-0"
-        style={{
-        "--bs-table-bg": "transparent",
-        "--bs-table-color": "white"
-    }}>
-        <thead>
-            <tr>
-                <th>Username</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-            <tbody>{userList}</tbody>
-        </Table>
-        </div>
-
-        <div style={{ height: "45%", overflowY: "auto"}}>
-        <Table aria-label="request" className="mt-4"
-        style={{
-        "--bs-table-bg": "transparent",
-        "--bs-table-color": "white"
-    }}>
-        <thead>
-            <tr>
-                <th>Request</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-            <tbody>{requestList}</tbody>
-        </Table>
-        </div>
-            </Offcanvas>
+            {jwt && (
+                <FriendsSidebar 
+                    isOpen={isOpenFriends} 
+                    toggle={toggleMenu} 
+                    username={username} 
+                    jwt={jwt} 
+                    id = { id}
+                />
+            )}
             
         </div>
     );
