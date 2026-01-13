a5635df6e73bdddaa2bc1f52d7e845c8b324362a
Colanin3 <joseaag05@gmail.com>
2026-01-13 19:29:38 +0100

    Spectate Friend

--- src/main/java/es/us/dp1/l6_3_24_25/Petris/match/repository/MatchRepository.java
+++ src/main/java/es/us/dp1/l6_3_24_25/Petris/match/repository/MatchRepository.java
@@ -1,11 +1,14 @@
 package es.us.dp1.l6_3_24_25.Petris.match.repository;
 
-import es.us.dp1.l6_3_24_25.Petris.match.model.Match;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import es.us.dp1.l6_3_24_25.Petris.match.model.Match;
 
 @Repository
 public interface MatchRepository extends CrudRepository<Match, Integer> {
@@ -16,4 +19,7 @@ public interface MatchRepository extends CrudRepository<Match, Integer> {
     Optional<Match> findByCodeAndEndedAtNull(String code);
 
     List<Match> findByEndedAtNullAndStartedAtNotNull();
+
+    @Query("SELECT m FROM Match m WHERE (m.player1.id = :id OR m.player2.id = :id) AND m.endedAt IS NULL")
+    Optional<Match> findMatchWithPlayerByIdAndEndedAtNull(@Param("id") Integer id);
 }
