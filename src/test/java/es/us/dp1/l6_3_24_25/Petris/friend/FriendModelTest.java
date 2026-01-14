d9bf315ca3c3817bd2e16ae0e61b1706d7928eb8
Colanin3 <joseaag05@gmail.com>
2026-01-14 14:10:02 +0100

    Test Friends

---
+++ src/test/java/es/us/dp1/l6_3_24_25/Petris/friend/FriendModelTest.java
@@ -0,0 +1,5 @@
+package es.us.dp1.l6_3_24_25.Petris.friend;
+
+public class FriendModelTest {
+
+}
---
+++ src/test/java/es/us/dp1/l6_3_24_25/Petris/friend/FriendServiceTest.java
@@ -0,0 +1,8 @@
+package es.us.dp1.l6_3_24_25.Petris.friend;
+
+public class FriendServiceTest {
+
+    // Tests for FriendService would go here
+
+    
+}
